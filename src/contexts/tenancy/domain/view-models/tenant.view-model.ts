2949232865f5b923447be2b7a4b4350882d7ec84
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2026-08-31 15:56:32 +0200

    refactor(tenancy): add toViewModel method in TenantTypeOrmMapper using builder pattern
    Implemented the toViewModel method in TenantTypeOrmMapper to convert TenantEntity instances into TenantViewModel objects. This enhancement improves code organization and maintains consistency with the builder pattern used in other mappers.

---
+++ src/contexts/tenancy/domain/view-models/tenant.view-model.ts
@@ -0,0 +1,16 @@
+import { ITenantMembershipPrimitives } from '@contexts/tenancy/domain/primitives/tenant-membership.primitives';
+import { ITenantPrimitives } from '@contexts/tenancy/domain/primitives/tenant.primitives';
+import { BaseViewModel } from '@sisques-labs/nestjs-kit';
+
+export class TenantViewModel extends BaseViewModel {
+  public readonly appId: string;
+  public readonly name: string;
+  public readonly slug: string;
+
+  constructor(props: ITenantPrimitives) {
+    super(props.id, props.createdAt, props.updatedAt);
+    this.appId = props.appId;
+    this.name = props.name;
+    this.slug = props.slug;
+  }
+}
